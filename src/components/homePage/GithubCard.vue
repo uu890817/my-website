b38ac4ce2694c3941199fa3b3dc25e8242d24e2e
裕(8600K) <uu890817@gmail.com>
2023-10-01 21:38:52 +0800

    更新時區造成的錯誤與添加專案簡介

--- src/components/homePage/GithubCard.vue
+++ src/components/homePage/GithubCard.vue
@@ -1,26 +1,39 @@
 <template>
     <div class="githubRepoWrap">
         <n-space justify="space-between" align="center">
-            <p class="repoName">
+            <p class="repoNameWrap">
                 <n-icon size="30" style="vertical-align: middle;">
                     <GitNetworkSharp />
                 </n-icon>
-                {{ props.repoData?.full_name }}
+
+                <span class="userName" @click="toWhosePage = fullName.userName; toGithubUserPage = true">{{
+                    fullName.userName }}</span> /
+                <span class="repoName"
+                    @click="toWhosePage = fullName.userName; toWhichRepo = fullName.repoName; toGithubRepoPage = true">{{
+                        fullName.repoName }}</span>
             </p>
 
-            <p>最後更新 : {{ lastUpdateDate }}</p>
+            <p>最後更新 : {{ lastUpdateDate }}(UTC+8)</p>
             <!-- {{ props.repoData?.created_at }}
             {{ props.repoData?.updated_at }} -->
         </n-space>
         <hr>
+        <div v-if="props.repoData?.description !== null">
+            專案簡介:
+            <div class=" description">{{ props.repoData?.description }}</div>
+        </div>
+        <div v-else>
+            專案簡介:
+            <div class=" description">本專案目前沒有簡介༼ つ ◕_◕ ༽つ</div>
+        </div>
+        <hr>
         <n-space justify="space-between" align="end">
             <n-space>
                 <n-avatar-group :options="contributorsOptions" :size="40" :max="10">
-                    <template #avatar="{ option: { name, src, url } }">
+                    <template #avatar="{ option: { name, src } }">
                         <n-tooltip>
                             <template #trigger>
-                                <a :href="url">
-                                    <n-avatar :src="src" /></a>
+                                <n-avatar :src="src" @click="toWhosePage = name; toGithubUserPage = true" />
                             </template>
                             {{ name }}
                         </n-tooltip>
@@ -32,11 +45,56 @@
                     </template>
                 </n-avatar-group>
             </n-space>
+
             <n-space justify="end">
-                <n-tag :bordered="false" type="warning" round v-for="(value, key) in usedLanguages" :key="value"
-                    style="vertical-align: bottom;">
-                    {{ key }}
-                </n-tag>
+                <div v-for="(value, key) in usedLanguages" :key="value">
+
+                    <n-tag v-if="key === 'Java'" :bordered="false"
+                        :color="{ color: '#493541', textColor: '#d48cb9', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'Vue'" :bordered="false" type="success" round style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'TypeScript'" :bordered="false" type="info" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'JavaScript'" :bordered="false"
+                        :color="{ color: '#5b4a32', textColor: '#d4c78c', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'HTML'" :bordered="false"
+                        :color="{ color: '#5b3232', textColor: '#ec5d5d', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'CSS'" :bordered="false"
+                        :color="{ color: '#313755', textColor: '#607dff', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'Python'" :bordered="false"
+                        :color="{ color: '#3973a4', textColor: '#d4c78c', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'PHP'" :bordered="false"
+                        :color="{ color: '#7377ad', textColor: '#272739', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else-if="key === 'C#'" :bordered="false"
+                        :color="{ color: '#6d287e', textColor: '#d04cf0', borderColor: '#1e496f' }" round
+                        style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                    <n-tag v-else :bordered="false" round style="vertical-align: bottom;">
+                        {{ key }}
+                    </n-tag>
+                </div>
             </n-space>
 
         </n-space>
@@ -44,10 +102,61 @@
 
     </div>
     <!-- {{ props.repoData }} -->
+
+
+    <n-modal v-model:show="toGithubUserPage">
+        <n-card style="width: 600px" :bordered="false" size="huge" role="dialog" aria-modal="true">
+
+            <span style="font-size: 20px; ">
+                <n-icon size="23" style="vertical-align: sub;">
+                    <LogOutOutline />
+                </n-icon>
+                {{ `即將前往 ${toWhosePage} 的Github頁面` }}
+            </span>
+            <template #footer>
+                <n-space justify="end">
+                    <n-button type="error" @click="toUserPage('cancle')">
+                        取消前往
+                    </n-button>
+                    <n-button type="warning" @click="toUserPage('newPage')">
+                        開新分頁
+                    </n-button>
+                    <n-button type="success" @click="toUserPage('go')">
+                        確認前往
+                    </n-button>
+                </n-space>
+            </template>
+        </n-card>
+    </n-modal>
+
+    <n-modal v-model:show="toGithubRepoPage">
+        <n-card style="width: 600px" :bordered="false" size="huge" role="dialog" aria-modal="true">
+
+            <span style="font-size: 20px; ">
+                <n-icon size="23" style="vertical-align: sub;">
+                    <LogOutOutline />
+                </n-icon>
+                {{ `即將前往 ${toWhosePage} / ${toWhichRepo} 的Github頁面` }}
+            </span>
+            <template #footer>
+                <n-space justify="end">
+                    <n-button type="error" @click="toRepoPage('cancle')">
+                        取消前往
+                    </n-button>
+                    <n-button type="warning" @click="toRepoPage('newPage')">
+                        開新分頁
+                    </n-button>
+                    <n-button type="success" @click="toRepoPage('go')">
+                        確認前往
+                    </n-button>
+                </n-space>
+            </template>
+        </n-card>
+    </n-modal>
 </template>
     
 <script setup lang='ts'>
-import { GitNetworkSharp } from "@vicons/ionicons5";
+import { GitNetworkSharp, LogOutOutline } from "@vicons/ionicons5";
 import { getUsedLanguage, getContributors } from "@/api/requests.ts";
 import { onMounted, ref, computed } from "vue";
 
@@ -55,10 +164,14 @@ const props = defineProps({
   repoData: Object,
 });
 
+const toGithubUserPage = ref(false);
+const toGithubRepoPage = ref(false);
+const toWhosePage = ref("");
+const toWhichRepo = ref("");
 const usedLanguages = ref({});
 const contributors = ref([]);
 const lastUpdateDate = computed(() => {
-  const dateTime = new Date(props.repoData?.updated_at);
+  const dateTime = new Date(new Date(props.repoData?.updated_at).getTime() - (8 * 60 * 60 * 1000));
 
   const year = dateTime.getFullYear();
   const month = (dateTime.getMonth() + 1).toString().padStart(2, "0"); // Month is 0-indexed
@@ -80,6 +193,24 @@ const contributorsOptions = computed(() => {
   });
   return data;
 });
+const fullName = computed(() => {
+  const name = props.repoData?.full_name;
+  if (name === undefined) {
+    return {
+      userName: undefined,
+      repoName: undefined,
+    };
+  } else {
+    const splitName = name.split("/");
+    const result = {
+      userName: splitName[0],
+      repoName: splitName[1],
+    };
+    return result;
+  }
+
+
+});
 
 
 const createDropdownOptions = (options: Array<{ name: string; src: string }>) =>
@@ -88,14 +219,46 @@ const createDropdownOptions = (options: Array<{ name: string; src: string }>) =>
     label: option.name
   }));
 
+
+const toUserPage = (type: string) => {
+  if (type === "cancle") {
+    toGithubUserPage.value = false;
+    return;
+  }
+  if (type === "newPage") {
+    window.open(`https://github.com/${toWhosePage.value}`);
+    toGithubUserPage.value = false;
+  }
+  if (type === "go") {
+    window.location.href = `https://github.com/${toWhosePage.value}`;
+    toGithubUserPage.value = false;
+
+  }
+};
+
+const toRepoPage = (type: string) => {
+  if (type === "cancle") {
+    toGithubRepoPage.value = false;
+    return;
+  }
+  if (type === "newPage") {
+    window.open(`https://github.com/${toWhosePage.value}/${toWhichRepo.value}`);
+    toGithubRepoPage.value = false;
+  }
+  if (type === "go") {
+    window.location.href = `https://github.com/${toWhosePage.value}/${toWhichRepo.value}`;
+    toGithubRepoPage.value = false;
+
+  }
+};
+
 onMounted(async () => {
 
   const getUsedLanguagesData = localStorage.getItem(`${props.repoData?.full_name}:usedLanguages`);
   const getUsedLanguagesTime = localStorage.getItem(`${props.repoData?.full_name}:usedLanguagesEfficientTime`);
   const getContributorsData = localStorage.getItem(`${props.repoData?.full_name}:contributors`);
   const getContributorsTime = localStorage.getItem(`${props.repoData?.full_name}:contributorsEfficientTime`);
-
-  if (getUsedLanguagesData !== null && getUsedLanguagesTime !== null && (new Date().getTime() - parseInt(getUsedLanguagesTime)) > 1800) {
+  if (getUsedLanguagesData !== null && getUsedLanguagesTime !== null && (new Date().getTime() - parseInt(getUsedLanguagesTime)) < 1800000) {
     usedLanguages.value = JSON.parse(getUsedLanguagesData);
   } else {
     const usedLanguagesResult = await getUsedLanguage(props.repoData?.full_name);
@@ -104,7 +267,7 @@ onMounted(async () => {
     usedLanguages.value = usedLanguagesResult;
   }
 
-  if (getContributorsData !== null && getContributorsTime !== null && (new Date().getTime() - parseInt(getContributorsTime)) > 1800) {
+  if (getContributorsData !== null && getContributorsTime !== null && (new Date().getTime() - parseInt(getContributorsTime)) < 1800000) {
     contributors.value = JSON.parse(getContributorsData);
   } else {
     const contributorsResult = await getContributors(props.repoData?.full_name);
@@ -117,7 +280,27 @@ onMounted(async () => {
 </script>
     
 <style>
+.userName,
 .repoName {
+    /* 禁止底線 */
+    text-decoration: none;
+    transition: all 0.2s;
+}
+
+.userName:hover,
+.repoName:hover {
+    color: rgb(121, 121, 166);
+    transition: all 0.2s;
+    cursor: pointer;
+
+}
+
+
+.n-avatar {
+    cursor: pointer;
+}
+
+.repoNameWrap {
     font-size: 20px;
     font-weight: bold;
 }
@@ -130,5 +313,25 @@ onMounted(async () => {
     border-radius: 10px;
     background-color: #262a32;
     /* box-shadow: 0 0 10px #16082c; */
+    transition: all 0.2s;
+
+}
+
+.githubRepoWrap:hover {
+    background-color: #2c303a;
+    transition: all 0.2s;
+}
+
+.description {
+    font-size: 16px;
+    /* 行高 */
+
+    line-height: 1.5;
+
+
+    margin-top: 5px;
+    padding: 20px;
+    background: #22262a;
+    /* border: 1px solid rgb(180, 180, 180); */
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,61 +1,61 @@
 <template>
-    <div class="homeWrap">
-        <div class="left animate__animated animate__bounceInLeft">
-            <ProfileComponent />
-        </div>
+  <div class="homeWrap">
+    <div class="left animate__animated animate__bounceInLeft">
+      <ProfileComponent />
+    </div>
 
-        <div class="right animate__animated animate__bounceInRight">
-            <div v-if="error403">
-                <n-space justify="space-between" align="center">
-                    <p style="font-size: 20px;">在Github上的專案(以下資料來自Github API):</p>
+    <div class="right animate__animated animate__bounceInRight">
+      <div v-if="error403">
+        <n-space justify="space-between" align="center">
+          <p style="font-size: 20px;">在Github上的專案(以下資料來自Github API):</p>
 
-                    <n-space>
-                        <n-select v-model:value="sortValue" :options="sortOptions" style="min-width: 200px;" />
-                        <n-popover trigger="hover">
-                            <template #trigger>
-                                <n-button strong secondary circle @click="changeOrder">
-                                    <template #icon>
-                                        <div v-if="(sortOrder === 'asc')">
-                                            <n-icon>
-                                                <ChevronUpOutline />
-                                            </n-icon>
-                                        </div>
-                                        <div v-else>
-                                            <n-icon>
-                                                <ChevronDownOutline />
-                                            </n-icon>
-                                        </div>
-
-                                    </template>
-                                </n-button>
-                            </template>
-                            <div v-if="(sortOrder === 'asc')"><span>目前: 早 ➔ 晚</span></div>
-                            <div v-else><span>目前: 晚 ➔ 早</span></div>
-
-                        </n-popover>
-
-                    </n-space>
-                </n-space>
-                <div v-if="reflash">
-                    <div v-for="repo in  sortedRepos " :key="repo.id">
-                        <GithubCard :repoData="repo" />
+          <n-space>
+            <n-select v-model:value="sortValue" :options="sortOptions" style="min-width: 200px;" />
+            <n-popover trigger="hover">
+              <template #trigger>
+                <n-button strong secondary circle @click="changeOrder">
+                  <template #icon>
+                    <div v-if="(sortOrder === 'asc')">
+                      <n-icon>
+                        <ChevronUpOutline />
+                      </n-icon>
                     </div>
-                    <n-space justify="center">
-                        <n-pagination v-model:page="nowPage" :page-count="pages" />
-                    </n-space>
-                </div>
-            </div>
-            <div v-else>
-                本IP目前已被Github API限制流量，因此部分畫面無法顯示，請過一陣子再嘗試，造成不便請見諒。
-                <br>
-                或是先到以下頁面參觀看看，感謝您的理解。
-            </div>
-            <!-- <img
+                    <div v-else>
+                      <n-icon>
+                        <ChevronDownOutline />
+                      </n-icon>
+                    </div>
+
+                  </template>
+                </n-button>
+              </template>
+              <div v-if="(sortOrder === 'asc')"><span>目前: 早 ➔ 晚</span></div>
+              <div v-else><span>目前: 晚 ➔ 早</span></div>
+
+            </n-popover>
+
+          </n-space>
+        </n-space>
+        <div v-if="reflash">
+          <div v-for="repo in  sortedRepos " :key="repo.id">
+            <GithubCard :repoData="repo" />
+          </div>
+          <n-space justify="center">
+            <n-pagination v-model:page="nowPage" :page-count="pages" />
+          </n-space>
+        </div>
+      </div>
+      <div v-else>
+        本IP目前已被Github API限制流量，因此部分畫面無法顯示，請一小時候再嘗試，造成不便請見諒。
+        <br>
+        或是先到以下頁面參觀看看，感謝您的理解。
+      </div>
+      <!-- <img
                 src="https://github-readme-stats.vercel.app/api?username=uu890817&theme=react&show_icons=true&hide_border=false&count_private=true">
             <img src="https://github-readme-streak-stats.herokuapp.com/?user=uu890817&theme=react&hide_border=false"> -->
 
-        </div>
     </div>
+  </div>
 </template>
     
 <script setup lang='ts'>
@@ -177,36 +177,36 @@ onMounted(async () => {
     
 <style scoped>
 .homeWrap {
-    /* 左右布局 */
-    position: relative;
-    width: 100%;
+  /* 左右布局 */
+  position: relative;
+  width: 100%;
 
 }
 
 .left {
-    /* 左布局 */
-    position: absolute;
-    left: 0;
-    width: 35%;
-    padding: 20px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: #3b3b51;
-    border-radius: 5px;
-    background-color: #2b2b36;
+  /* 左布局 */
+  position: absolute;
+  left: 0;
+  width: 35%;
+  padding: 20px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #3b3b51;
+  border-radius: 5px;
+  background-color: #2b2b36;
 }
 
 .right {
-    /* 右布局 */
-    position: absolute;
-    right: 0;
-    width: 60%;
-    padding: 10px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: #3b3b51;
-    border-radius: 5px;
-    background-color: #2b2b36;
+  /* 右布局 */
+  position: absolute;
+  right: 0;
+  width: 60%;
+  padding: 10px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #3b3b51;
+  border-radius: 5px;
+  background-color: #2b2b36;
 
 
 }
